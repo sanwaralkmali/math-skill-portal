3d568efa82383ebc3daf077b46864786cce61301
abdurrahmanninja <mtnshsalah@gmail.com>
2025-07-21 00:53:04 +0300

    Fixing the deploment issue 2

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/skills" replace />} />
+          <Route path="/" element={<div />} />
           <Route path="/skills/:skillName" element={<SkillPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
