5a72b3cdd864ab1adbe75e364d263b2802edb6e2
abdurrahmanninja <mtnshsalah@gmail.com>
2025-07-21 00:58:18 +0300

    Deploy issue

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import SkillPage from "./pages/SkillPage";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div />} />
+          <Route path="/" element={<Index />} />
           <Route path="/skills/:skillName" element={<SkillPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
